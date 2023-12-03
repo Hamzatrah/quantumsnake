5dbac7669f2d415bc20d67f5b6f08770f84fc2fa
Hamzatrah <41709149+Hamzatrah@users.noreply.github.com>
2023-12-03 16:53:13 +0100

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,27 @@
+body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    font-family: Arial, sans-serif;
+}
+
+canvas {
+    border: 1px solid black;
+}
+
+#gameInfo, #mobileControls {
+    margin-top: 20px;
+}
+
+button {
+    margin: 5px;
+    padding: 10px;
+    font-size: 16px;
+}
+
+#mobileControls button {
+    width: 80px;
+}
